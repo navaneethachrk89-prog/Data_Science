173e526012f1498c1bdf8ac19d8e874c959c24f6
navaneethachrk89-prog <navaneetha.chrk89@gmail.com>
2026-07-09 21:36:07 +0530

    Add files via upload

---
+++ 09July_Polynomial regression.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 20:28:23 2026
+
+@author: Shiva
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataset = pd.read_csv(r"C:\Users\Shiva\OneDrive\Desktop\FSDS\Daily Notes\09July\9th - Poly\1.POLYNOMIAL REGRESSION\emp_sal.csv")
+
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
+plt.scatter(X,y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression graph')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+lin_model_pred = lin_reg.predict([[6.5]])
+lin_model_pred
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=6)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+plt.scatter(X,y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+print(poly_model_pred)
+
